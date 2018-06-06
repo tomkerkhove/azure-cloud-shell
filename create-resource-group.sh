91bea36b6152315080d6fc005055eedd37ae0648
Tom Kerkhove <kerkhove.tom@gmail.com>
2018-06-06 14:33:50 +0200

    Update create-resource-group.sh

--- create-resource-group.sh
+++ create-resource-group.sh
@@ -3,8 +3,8 @@ echo "Subscription Name: $1"
 echo "Resource Group Name: $2"
 echo "Resource Group Location: $3"
 
-az account set -s "$1"
+az account set -s "$1" --debug
 echo "Switched to subscription $1"
 
-az group create --name "$2" --location "$3"
+az group create --name "$2" --location "$3" --debug
 echo "Created resource group $2"
