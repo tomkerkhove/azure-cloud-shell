6e00ca99b905115b00dbfcb896a266bc740f5d95
Tom Kerkhove <kerkhove.tom@gmail.com>
2018-06-06 14:28:40 +0200

    Update create-resource-group.sh

--- create-resource-group.sh
+++ create-resource-group.sh
@@ -3,5 +3,5 @@ echo "Subscription Name: $1"
 echo "Resource Group Name: $2"
 echo "Resource Group Location: $3"
 
-az account set -s $1
-az group create --name $2 --location $3
+az account set -s "$1"
+az group create --name "$2" --location "$3"
